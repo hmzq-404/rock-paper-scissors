941b6ae708aeeb8713f2350e62277cfe34e2b88c
hmzq-404 <66130102+hmzq-404@users.noreply.github.com>
2023-07-30 21:46:17 +0500

    Add UI to game

--- script.js
+++ script.js
@@ -8,63 +8,92 @@ function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function getPlayerChoice() {
-    let choice = "";
-    while (!validChoices.includes(choice)) {
-        let playerInput = prompt("Rock, Paper, Scissors, Shoot!");
-        choice = playerInput ? playerInput.toLowerCase() : "";
-    }
-    return choice;
-}
-
 function getComputerChoice() {
     return validChoices[getRandomNumber(0, validChoices.length)];
 }
 
-function playRound(playerSelection, computerSelection) {
+function getRoundResult(playerSelection, computerSelection) {
     let playerSelectionWeight = validChoices.indexOf(playerSelection);
     let computerSelectionWeight = validChoices.indexOf(computerSelection);
 
     // Tie
     if (playerSelectionWeight === computerSelectionWeight) {
-        return "It's a tie!";
+        return `Round #${roundNumber} is a tie!`;
     }
 
     // Player won
     else if (playerSelectionWeight === computerSelectionWeight + 1 || playerSelectionWeight === 0 && computerSelectionWeight === 2) {
         playerScore++;
-        return "You won!";
+        return `You won Round #${roundNumber}!`;
     } 
 
     // Player lost
     else {
         computerScore++;
-        return "You lost!"
+        return `You lost Round #${roundNumber}!`;
     }
 }
 
-function game() {
-    let playerSelection;
-    let computerSelection;
-    let roundResult;
 
-    while (playerScore < 5 && computerScore < 5) {
-        roundNumber++;
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
-        roundResult = playRound(playerSelection, computerSelection);
+function playRound(event) {
+    roundNumber++;
+    let playerChoice = event.target.getAttribute("data-choice");
+    let computerChoice = getComputerChoice();
+    let roundResult = getRoundResult(playerChoice, computerChoice);
 
-        console.log(`Round #${roundNumber}: ${playerSelection} vs ${computerSelection}`);
-        console.log(`Your score: ${playerScore}`);
-        console.log(`Computer's score: ${computerScore}`);
-        console.log(roundResult);
-    }
+    // Update DOM
+    const roundNumberHeading = document.querySelector(".round-number");
+    roundNumberHeading.innerText = `Round #${roundNumber}`;
+
+    const playerScoreHeading = document.querySelector(".player-score");
+    playerScoreHeading.innerText = `Your score: ${playerScore}`;
+    const computerScoreHeading = document.querySelector(".computer-score");
+    computerScoreHeading.innerText = `Computer's score: ${computerScore}`;
+
+    const playerChoiceHeading = document.querySelector(".player-choice");
+    playerChoiceHeading.innerText = `You chose: ${playerChoice}`;
+    const computerChoiceHeading = document.querySelector(".computer-choice");
+    computerChoiceHeading.innerText = `Computer chose: ${computerChoice}`;
+
+    const resultHeading = document.querySelector(".result");
 
     if (playerScore === 5) {
-        return "You won!";
+        resultHeading.innerText = "🎉 You won!";
+    } else if (computerScore === 5) {
+        resultHeading.innerText = "😔 You lost!";
     } else {
-        return "You lost!";
+        resultHeading.innerText = roundResult;
     }
 }
 
-console.log("Final Result: " + game());
+
+const choiceImages = document.querySelectorAll(".choice-img");
+
+choiceImages.forEach((image) => {
+    image.addEventListener("click", (event) => {
+        if (playerScore < 5 && computerScore < 5) playRound(event);
+    });
+});
+
+const resetButton = document.querySelector(".reset-button");
+
+resetButton.addEventListener("click", () => {
+    playerScore = 0;
+    computerScore = 0;
+    roundNumber = 0;
+    const roundNumberHeading = document.querySelector(".round-number");
+    roundNumberHeading.innerText = `Round #${roundNumber}`;
+
+    const playerScoreHeading = document.querySelector(".player-score");
+    playerScoreHeading.innerText = `Your score: ${playerScore}`;
+    const computerScoreHeading = document.querySelector(".computer-score");
+    computerScoreHeading.innerText = `Computer's score: ${computerScore}`;
+
+    const playerChoiceHeading = document.querySelector(".player-choice");
+    playerChoiceHeading.innerText = "";
+    const computerChoiceHeading = document.querySelector(".computer-choice");
+    computerChoiceHeading.innerText = "";
+
+    const resultHeading = document.querySelector(".result");
+    resultHeading.innerText = "";
+})
